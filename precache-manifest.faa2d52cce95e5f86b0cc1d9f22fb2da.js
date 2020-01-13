45569ededc916f6cdac6c90d1385eb1b13b91bd5
Rick <rick.carnes@gmail.com>
2020-01-13 01:20:08 -0500

    Updates

---
+++ precache-manifest.faa2d52cce95e5f86b0cc1d9f22fb2da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9260c340606b1f9fbe6be999bdc001",
+    "url": "/comcast-weekend-app/index.html"
+  },
+  {
+    "revision": "dc3775f220bf6e4706ed",
+    "url": "/comcast-weekend-app/static/css/main.7cf58671.chunk.css"
+  },
+  {
+    "revision": "bb736372a1a6505f553e",
+    "url": "/comcast-weekend-app/static/js/2.409df4f8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/comcast-weekend-app/static/js/2.409df4f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc3775f220bf6e4706ed",
+    "url": "/comcast-weekend-app/static/js/main.511f42d8.chunk.js"
+  },
+  {
+    "revision": "40531a17f6dcb685d26e",
+    "url": "/comcast-weekend-app/static/js/runtime-main.3ac3c668.js"
+  }
+]);
